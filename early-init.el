2c44312950de95b7c3a6bd33d0b8237f2f00ed5e
Alister Sanders <alister@sugol.org>
2025-02-09 21:26:08 +1300

    refactor(early-init): remove unused load paths

--- early-init.el
+++ early-init.el
@@ -1,8 +1,5 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/coremacs"))
 
-(dolist (dirname '("nano-tools" "nano-modeline"))
-  (add-to-list 'load-path (expand-file-name dirname "~/.emacs.d/nano/")))
-
 (add-to-list 'default-frame-alist
              '(drag-with-header-line . t))
 
